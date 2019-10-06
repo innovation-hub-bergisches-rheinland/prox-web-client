ef5c57d9f37ff896aee3f1435830298b997620cc
Peter Topfstedt <44511324+xLacrima@users.noreply.github.com>
2019-10-06 16:38:16 +0200

    Revert "Angular Update WIP"
    This reverts commit 68cd90f88bd2fa6b14a920afa72f34bb39aabe42.

--- src/app/components/project-dialog/project-dialog.component.ts
+++ src/app/components/project-dialog/project-dialog.component.ts
@@ -44,8 +44,8 @@ export class ProjectDialogComponent implements OnInit {
 
   recommendedTags: Tag[] = [];
 
-  @ViewChild('tagInput', { static: false }) tagInput: ElementRef<HTMLInputElement>;
-  @ViewChild('tagAuto', { static: false }) tagAutocomplete: MatAutocomplete;
+  @ViewChild('tagInput') tagInput: ElementRef<HTMLInputElement>;
+  @ViewChild('tagAuto') tagAutocomplete: MatAutocomplete;
 
   constructor(
     public projectDialogRef: MatDialogRef<ProjectDialogComponent>,
