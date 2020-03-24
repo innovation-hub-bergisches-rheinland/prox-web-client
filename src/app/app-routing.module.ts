51c1eb66b547034e30ac281d51f0df29979c06e3
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2020-03-24 15:57:44 +0100

    Fix child routes for Angular 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,51 +18,61 @@ const routes: Routes = [
     children: [
       {
         path: 'home',
-        loadChildren: () =>
-          import('@modules/home/home.module').then(m => m.HomeModule)
+        // loadChildren: () =>
+        //   import('@modules/home/home.module').then(m => m.HomeModule)
+        loadChildren: '@modules/home/home.module#HomeModule'
       },
       {
         path: 'projects',
-        loadChildren: () =>
-          import('@modules/project/project.module').then(m => m.ProjectModule)
+        // loadChildren: () =>
+        //   import('@modules/project/project.module').then(m => m.ProjectModule)
+        loadChildren: '@modules/project/project.module#ProjectModule'
       },
       {
         path: 'study-courses',
-        loadChildren: () =>
-          import('@modules/study-course/study-course.module').then(
-            m => m.StudyCourseModule
-          )
+        // loadChildren: () =>
+        //   import('@modules/study-course/study-course.module').then(
+        //     m => m.StudyCourseModule
+        //   )
+        loadChildren:
+          '@modules/study-course/study-course.module#StudyCourseModule'
       },
       {
         path: 'contact',
-        loadChildren: () =>
-          import('@modules/contact/contact.module').then(m => m.ContactModule)
+        // loadChildren: () =>
+        //   import('@modules/contact/contact.module').then(m => m.ContactModule)
+        loadChildren: '@modules/contact/contact.module#ContactModule'
       },
       {
         path: 'impressum',
-        loadChildren: () =>
-          import('@modules/impressum/impressum.module').then(
-            m => m.ImpressumModule
-          )
+        // loadChildren: () =>
+        //   import('@modules/impressum/impressum.module').then(
+        //     m => m.ImpressumModule
+        //   )
+        loadChildren: '@modules/impressum/impressum.module#ImpressumModule'
       },
       {
         path: 'privacy',
-        loadChildren: () =>
-          import('@modules/privacy/privacy.module').then(m => m.PrivacyModule)
+        // loadChildren: () =>
+        //   import('@modules/privacy/privacy.module').then(m => m.PrivacyModule)
+        loadChildren: '@modules/privacy/privacy.module#PrivacyModule'
       },
       {
         path: 'disclaimer',
-        loadChildren: () =>
-          import('@modules/disclaimer/disclaimer.module').then(
-            m => m.DisclaimerModule
-          )
+        // loadChildren: () =>
+        //   import('@modules/disclaimer/disclaimer.module').then(
+        //     m => m.DisclaimerModule
+        //   )
+        loadChildren: '@modules/disclaimer/disclaimer.module#DisclaimerModule'
       },
       {
         path: 'page-not-found',
-        loadChildren: () =>
-          import('@modules/page-not-found/page-not-found.module').then(
-            m => m.PageNotFoundModule
-          )
+        // loadChildren: () =>
+        //   import('@modules/page-not-found/page-not-found.module').then(
+        //     m => m.PageNotFoundModule
+        //   )
+        loadChildren:
+          '@modules/page-not-found/page-not-found.module#PageNotFoundModule'
       }
     ]
   },
