c09686edf56df51bc67e08ff7e31eeef48fc0ba1
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2020-03-31 14:41:30 +0200

    Refactor module sorting hack

--- src/app/data/schema/study-course.resource.ts
+++ src/app/data/schema/study-course.resource.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs';
 import { CustomResource } from './custom-resource.resource';
 import { Module } from './module.resource';
+import { map } from 'rxjs/operators';
 
 export class StudyCourse extends CustomResource {
   id: string;
@@ -13,33 +14,7 @@ export class StudyCourse extends CustomResource {
   }
 
   getModules(): Observable<Module[]> {
-    // THIS DOES NOT WORK
-    // There is a bug that appends a slash between the URL and the params
-    // when you call getRelationArray with HalOptions
-    // let options: HalOptions = {sort: [{path: 'name', order: 'ASC'}]};
-    // return this.getRelationArray(Module, 'modules', undefined, options);
     return this.getRelationArray(Module, 'modules');
-  }
-
-  getAndSetModuleArray(): Promise<Module[]> {
-    return new Promise<Module[]>((resolve, reject) => {
-      this.getModules().subscribe(
-        tmpModules => (this.modules = tmpModules),
-        () => reject(),
-        () => {
-          this.modules.sort((a, b) => {
-            if (a.name < b.name) {
-              return -1;
-            }
-            if (a.name > b.name) {
-              return 1;
-            }
-            return 0;
-          });
-          resolve(this.modules);
-        }
-      );
-    });
   }
 
   getParentStudyCourse(): Observable<StudyCourse> {
--- src/app/modules/study-course/page/study-course-details/study-course-details.component.ts
+++ src/app/modules/study-course/page/study-course-details/study-course-details.component.ts
@@ -3,6 +3,10 @@ import { Component, OnInit } from '@angular/core';
 import { StudyCourse } from '@data/schema/study-course.resource';
 import { StudyCourseService } from '@data/service/study-course.service';
 import { ActivatedRoute } from '@angular/router';
+import { HalOptions, Sort } from 'angular4-hal';
+import { Module } from '@data/schema/module.resource';
+import { SortOrder } from 'angular4-hal/src/sort';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-study-course-details',
@@ -16,18 +20,32 @@ export class StudyCourseDetailsComponent implements OnInit {
   constructor(
     private studyCourseService: StudyCourseService,
     private route: ActivatedRoute
-  ) {
-    this.route.params.subscribe(params => (this.studyCourseID = params.id));
-  }
+  ) {}
 
   ngOnInit() {
+    this.route.params.subscribe(params => (this.studyCourseID = params.id));
     this.getStudyCourse();
   }
 
   private getStudyCourse() {
     this.studyCourseService.get(this.studyCourseID).subscribe(studyCourse => {
       this.studyCourse = studyCourse;
-      this.studyCourse.getAndSetModuleArray().then();
+      studyCourse
+        .getModules()
+        .pipe(
+          map(modules =>
+            modules.sort((a, b) => {
+              if (a.name < b.name) {
+                return -1;
+              }
+              if (a.name > b.name) {
+                return 1;
+              }
+              return 0;
+            })
+          )
+        )
+        .subscribe(modules => (studyCourse.modules = modules));
     });
   }
 }
