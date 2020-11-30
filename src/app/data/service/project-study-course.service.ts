1f8497b5829c15d8486b486ca63401741a77d306
Tobias Hund <tobias.hund@innovation-hub.de>
2020-11-30 15:03:57 +0100

    port studycourseservice to OpenAPI implementation

--- src/app/data/service/project-study-course.service.ts
+++ src/app/data/service/project-study-course.service.ts
@@ -5,16 +5,40 @@ import { Observable } from 'rxjs';
 
 import { StudyCourse } from '@data/schema/study-course.resource';
 import { HalRestService } from './base/hal-crud-rest-service';
+import { StudyCourseEntityService } from './openapi/project-service/studyCourseEntity.service';
+import { map } from 'rxjs/operators';
+import { Module } from '@data/schema/module.resource';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectStudyCourseService extends HalRestService<StudyCourse> {
-  constructor(injector: Injector) {
+  constructor(
+    injector: Injector,
+    private projectStudyCourseService: StudyCourseEntityService
+  ) {
     super(StudyCourse, 'projectStudyCourses', injector);
   }
 
+  findAllModulesOfStudyCourse(id: any): Observable<Module[]> {
+    return this.projectStudyCourseService.studyCourseModulesUsingGET(id).pipe(
+      map(m =>
+        m._embedded.projectModules.map(m2 => {
+          const module = new Module();
+          return Object.assign(module, m2);
+        })
+      )
+    );
+  }
+
   getAllProjectStudyCourses(): Observable<StudyCourse[]> {
-    return this.getAll();
+    return this.projectStudyCourseService.findAllStudyCourseUsingGET().pipe(
+      map(s =>
+        s._embedded.projectStudyCourses.map(s2 => {
+          const sc = new StudyCourse();
+          return Object.assign(sc, s2);
+        })
+      )
+    );
   }
 }
