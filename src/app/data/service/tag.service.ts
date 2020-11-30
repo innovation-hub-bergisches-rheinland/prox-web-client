684410c29b446f78c43976565a7bd6db64992e2b
Tobias Hund <tobias.hund@innovation-hub.de>
2020-11-30 13:32:46 +0100

    code style optimization

--- src/app/data/service/tag.service.ts
+++ src/app/data/service/tag.service.ts
@@ -1,12 +1,9 @@
-import { Inject, Injectable, Injector } from '@angular/core';
-
-import { RestService } from 'angular4-hal';
+import { Injectable, Injector } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Tag } from '@data/schema/tag.resource';
 import { HalRestService } from './base/hal-crud-rest-service';
 import { map, tap } from 'rxjs/operators';
-import { environment } from '@env';
 import { TagEntityService } from './openapi/tag-service/tagEntity.service';
 
 @Injectable({
@@ -25,7 +22,6 @@ export class TagService extends HalRestService<Tag> {
     tagName: string,
     exactMatch: boolean = true
   ): Observable<Tag[]> {
-    const options = { params: [{ key: 'tagName', value: tagName }] };
     if (exactMatch) {
       return this.tagEntityService
         .findByTagNameTagNameIgnoreCaseTagUsingGET(tagName)
@@ -43,7 +39,5 @@ export class TagService extends HalRestService<Tag> {
       map(t => t._embedded.tags as Tag[]),
       tap(t => console.log(t))
     );
-    const options = { params: [{ key: 'tagIds', value: tagIds }] };
-    return this.search('tagRecommendations', options);
   }
 }
