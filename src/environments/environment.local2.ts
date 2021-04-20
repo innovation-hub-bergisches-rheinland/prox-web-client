b29bdb08c8f937858be0d7911f684b46cd291b97
Tobias Hund <tobias.hund@innovation-hub.de>
2021-04-20 16:43:44 +0200

    create different local environemnts

---
+++ src/environments/environment.local2.ts
@@ -0,0 +1,14 @@
+import { KeycloakConfig } from 'keycloak-js';
+import { Environment } from './IEnvironment';
+
+const keycloakConfig: KeycloakConfig = {
+  url: 'https://login.archi-lab.io/auth',
+  realm: 'archilab',
+  clientId: 'prox-web-client'
+};
+
+export const environment: Environment = {
+  production: false,
+  keycloakConfig,
+  apiUrl: 'http://127.0.0.1:8081'
+};
