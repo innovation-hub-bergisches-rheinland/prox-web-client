1e925439d360bd6afb9fc8f856de4c08d29f4218
Tobias Hund <tobias.hund@innovation-hub.de>
2021-10-17 13:09:25 +0200

    Separate Material Module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
-import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   declarations: [ConfirmationDialogComponent],
