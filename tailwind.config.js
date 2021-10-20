f8fe1f48962d7765fedf6da79ba6ef4ba7a63b7d
Tobias Hund <tobias.hund@innovation-hub.de>
2021-10-20 23:08:17 +0200

    Tweak tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@
 module.exports = {
   prefix: '',
   purge: {
+    enabled: process.env.NODE_ENV === 'production',
     content: ['./src/**/*.{html,ts}']
   },
   darkMode: 'class', // or 'media' or 'class'
