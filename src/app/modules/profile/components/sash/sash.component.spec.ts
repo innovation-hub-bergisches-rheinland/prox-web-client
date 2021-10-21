74c09b8924410753033850e87af114ce7030c0da
Tobias Hund <tobias.hund@innovation-hub.de>
2021-10-21 17:34:33 +0200

    Add Sash Component

---
+++ src/app/modules/profile/components/sash/sash.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SashComponent } from './sash.component';
+
+describe('SashComponent', () => {
+  let component: SashComponent;
+  let fixture: ComponentFixture<SashComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SashComponent]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SashComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -3,9 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { SharedModule } from '@shared/shared.module';
+import {
+  SashComponent,
+  SashContentDirective,
+  SashTitleDirective
+} from './components/sash/sash.component';
+import { ExamplePageComponent } from './pages/example-page/example-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SashComponent,
+    SashContentDirective,
+    SashTitleDirective,
+    ExamplePageComponent
+  ],
   imports: [CommonModule, ProfileRoutingModule, SharedModule]
 })
 export class ProfileModule {}
