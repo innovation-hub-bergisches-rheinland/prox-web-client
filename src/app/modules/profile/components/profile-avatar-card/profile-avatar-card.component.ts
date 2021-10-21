a8d927d4652d784d3f02eaca262a854fb6e08749
Tobias Hund <tobias.hund@innovation-hub.de>
2021-10-21 17:40:33 +0200

    Add Profile avatar card

---
+++ src/app/modules/profile/components/profile-avatar-card/profile-avatar-card.component.ts
@@ -0,0 +1,27 @@
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input
+} from '@angular/core';
+
+@Component({
+  selector: 'profile-avatar-card',
+  templateUrl: './profile-avatar-card.component.html',
+  styleUrls: ['./profile-avatar-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProfileAvatarCardComponent implements OnInit {
+  @Input()
+  avatar: string;
+
+  @Input()
+  title: string;
+
+  @Input()
+  subtitle?: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -9,13 +9,15 @@ import {
   SashTitleDirective
 } from './components/sash/sash.component';
 import { ExamplePageComponent } from './pages/example-page/example-page.component';
+import { ProfileAvatarCardComponent } from '@modules/profile/components/profile-avatar-card/profile-avatar-card.component';
 
 @NgModule({
   declarations: [
     SashComponent,
     SashContentDirective,
     SashTitleDirective,
-    ExamplePageComponent
+    ExamplePageComponent,
+    ProfileAvatarCardComponent
   ],
   imports: [CommonModule, ProfileRoutingModule, SharedModule]
 })
