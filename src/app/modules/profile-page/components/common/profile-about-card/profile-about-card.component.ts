c133eda8dc3ccd7070f4f1902604260378642eb5
Tobias Hund <tobias.hund@innovation-hub.de>
2021-10-21 08:19:58 +0200

    Add fontawesome and dummy information

--- src/app/modules/profile-page/components/common/profile-about-card/profile-about-card.component.ts
+++ src/app/modules/profile-page/components/common/profile-about-card/profile-about-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { faAddressCard } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'profile-about-card',
@@ -7,6 +8,8 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfileAboutCardComponent implements OnInit {
+  faAdressCard = faAddressCard;
+
   constructor() {}
 
   ngOnInit(): void {}
