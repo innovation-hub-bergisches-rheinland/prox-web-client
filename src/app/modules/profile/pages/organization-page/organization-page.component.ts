ed9003b8c587191e0646920bb13f9f502bd60785
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-10-26 19:47:15 +0200

    Use real values

--- src/app/modules/profile/pages/organization-page/organization-page.component.ts
+++ src/app/modules/profile/pages/organization-page/organization-page.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { CompanyProfileService } from '@data/service/company-profile.service';
 import { Company } from '@data/schema/openapi/company-profile-service/company';
 import { Language } from '@data/schema/openapi/company-profile-service/language';
-import { Observable } from 'rxjs';
+import { forkJoin, Observable } from 'rxjs';
 import { environment } from '@env';
 import { SocialMedia } from '@modules/profile/components/profile-avatar-card/profile-avatar-card.component';
 import { FocusSubject } from '@modules/profile/components/profile-focus-areas/profile-focus-subjects.component';
@@ -20,6 +20,7 @@ export class OrganizationPageComponent implements OnInit {
   logo: string;
   socialMedia: SocialMedia;
   focusSubjects: FocusSubject[];
+  languages: Language[];
 
   get company(): Company {
     return this._company;
@@ -35,33 +36,42 @@ export class OrganizationPageComponent implements OnInit {
       .pipe(
         map(route => route.id),
         mergeMap(id => {
-          return this.companyProfileService.getCompanyById(id);
+          return forkJoin({
+            company: this.companyProfileService.getCompanyById(id),
+            languages: this.companyProfileService.getCompanyLanguages(id)
+          });
         })
       )
       .subscribe({
         next: res => {
-          this._company = res;
-          this.logo = `${environment.apiUrl}/companies/${res.id}/logo`;
+          this._company = res.company;
+          this.logo = `${environment.apiUrl}/companies/${this._company.id}/logo`;
           this.companyProfileService
             .getCompanyLanguages(this._company.id)
             .subscribe(langs => {
               this._languages = langs;
             });
           this.socialMedia = {
-            facebook: res.socialMedia.find(sm => sm.type === 'FACEBOOK')
+            facebook: this._company.socialMedia.find(
+              sm => sm.type === 'FACEBOOK'
+            )?.account,
+            instagram: this._company.socialMedia.find(
+              sm => sm.type === 'INSTAGRAM'
+            )?.account,
+            xing: this._company.socialMedia.find(sm => sm.type === 'XING')
               ?.account,
-            instagram: res.socialMedia.find(sm => sm.type === 'INSTAGRAM')
+            twitter: this._company.socialMedia.find(sm => sm.type === 'TWITTER')
               ?.account,
-            xing: res.socialMedia.find(sm => sm.type === 'XING')?.account,
-            twitter: res.socialMedia.find(sm => sm.type === 'TWITTER')?.account,
-            linkedIn: res.socialMedia.find(sm => sm.type === 'LINKEDIN')
-              ?.account
+            linkedIn: this._company.socialMedia.find(
+              sm => sm.type === 'LINKEDIN'
+            )?.account
           };
-          this.focusSubjects = res.branches.map(b => {
+          this.focusSubjects = this._company.branches.map(b => {
             return {
               subject: b.branchName
             };
           });
+          this.languages = res.languages;
         },
         error: err => {
           console.error(err);
