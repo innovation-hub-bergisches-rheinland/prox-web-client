14be7f0bc880f213393f9bb16d56e7d84d01b9f6
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-03-17 14:57:05 +0100

    chore: add dummy information to dummy org

--- src/app/modules/organization/pages/organization-profile/organization-profile.component.ts
+++ src/app/modules/organization/pages/organization-profile/organization-profile.component.ts
@@ -7,6 +7,9 @@ import { Observable } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Profile, Sash } from '@modules/profile/pages/profile-page/profile-page.component';
 import { faBullseye } from '@fortawesome/free-solid-svg-icons';
+import { AvailableProject } from '@modules/profile/components/profile-projects-card/profile-projects-card.component';
+import { AvailableJob } from '@modules/profile/components/profile-jobs-card/profile-jobs-card.component';
+import { ProjectHistoryItem } from '@modules/profile/components/profile-project-history/profile-project-history-item/profile-project-history-item.component';
 
 @Component({
   selector: 'app-organization-profile',
@@ -17,6 +20,77 @@ export class OrganizationProfileComponent implements OnInit {
   organization$: Observable<Organization>;
   avatar$: Observable<string>;
   faBullseye = faBullseye;
+
+  // TODO: Remove after demo
+  dummyAvailableProjects: AvailableProject[] = [
+    {
+      id: '383bd84e-e9b2-4c0e-8998-047c691cb242',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      modules: ['BA', 'PP']
+    },
+    {
+      id: '383bd84e-e9b2-4c0e-8998-047c691cb242',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      modules: ['BA', 'MA']
+    },
+    {
+      id: '383bd84e-e9b2-4c0e-8998-047c691cb242',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      modules: ['TP', 'IP']
+    },
+    {
+      id: '383bd84e-e9b2-4c0e-8998-047c691cb242',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      modules: ['WIP', 'MA', 'BA', 'PP']
+    }
+  ];
+  dummyJobs: AvailableJob[] = [
+    {
+      id: '383bd84e-e9b2-4c0e-8998-047c691cb242',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      levels: ['Berufseinsteiger']
+    },
+    {
+      id: '383bd84e-e9b2-4c0e-8998-047c691cb242',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      levels: ['Berufseinsteiger', 'Berufserfahrene']
+    },
+    {
+      id: '383bd84e-e9b2-4c0e-8998-047c691cb242',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      levels: ['Berufserfahrene']
+    }
+  ];
+  dummyHistoryItems: ProjectHistoryItem[] = [
+    {
+      id: '383bd84e-e9b2-4c0e-8998-047c691cb242',
+      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam commodo neque nibh, eget lacinia quam suscipit quis. In fermentum maximus mi. Proin ut scelerisque sapien. Integer mollis, elit id venenatis tincidunt, nisl mi mollis leo, in gravida justo velit consectetur purus. Proin ornare condimentum purus eu vestibulum. Curabitur lorem mauris, dapibus at enim at, sagittis gravida eros. Pellentesque magna nisi, vestibulum pulvinar mi ac, suscipit tincidunt tortor. Quisque condimentum dapibus congue. Morbi in ligula at leo sagittis sollicitudin. Mauris non est tincidunt, viverra lectus sed, vulputate lectus. Aliquam blandit feugiat velit ut pretium.',
+      supervisor: 'Prof. Dr. Erika Musterfrau, Prof. Dr. Max Mustermann'
+    },
+    {
+      id: '383bd84e-e9b2-4c0e-8998-047c691cb242',
+      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam commodo neque nibh, eget lacinia quam suscipit quis. In fermentum maximus mi. Proin ut scelerisque sapien. Integer mollis, elit id venenatis tincidunt, nisl mi mollis leo, in gravida justo velit consectetur purus. Proin ornare condimentum purus eu vestibulum. Curabitur lorem mauris, dapibus at enim at, sagittis gravida eros. Pellentesque magna nisi, vestibulum pulvinar mi ac, suscipit tincidunt tortor. Quisque condimentum dapibus congue. Morbi in ligula at leo sagittis sollicitudin. Mauris non est tincidunt, viverra lectus sed, vulputate lectus. Aliquam blandit feugiat velit ut pretium.',
+      supervisor: 'Prof. Dr. Erika Musterfrau, Prof. Dr. Max Mustermann'
+    },
+    {
+      id: '383bd84e-e9b2-4c0e-8998-047c691cb242',
+      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam commodo neque nibh, eget lacinia quam suscipit quis. In fermentum maximus mi. Proin ut scelerisque sapien. Integer mollis, elit id venenatis tincidunt, nisl mi mollis leo, in gravida justo velit consectetur purus. Proin ornare condimentum purus eu vestibulum. Curabitur lorem mauris, dapibus at enim at, sagittis gravida eros. Pellentesque magna nisi, vestibulum pulvinar mi ac, suscipit tincidunt tortor. Quisque condimentum dapibus congue. Morbi in ligula at leo sagittis sollicitudin. Mauris non est tincidunt, viverra lectus sed, vulputate lectus. Aliquam blandit feugiat velit ut pretium.',
+      supervisor: 'Prof. Dr. Erika Musterfrau, Prof. Dr. Max Mustermann'
+    },
+    {
+      id: '383bd84e-e9b2-4c0e-8998-047c691cb242',
+      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam commodo neque nibh, eget lacinia quam suscipit quis. In fermentum maximus mi. Proin ut scelerisque sapien. Integer mollis, elit id venenatis tincidunt, nisl mi mollis leo, in gravida justo velit consectetur purus. Proin ornare condimentum purus eu vestibulum. Curabitur lorem mauris, dapibus at enim at, sagittis gravida eros. Pellentesque magna nisi, vestibulum pulvinar mi ac, suscipit tincidunt tortor. Quisque condimentum dapibus congue. Morbi in ligula at leo sagittis sollicitudin. Mauris non est tincidunt, viverra lectus sed, vulputate lectus. Aliquam blandit feugiat velit ut pretium.',
+      supervisor: 'Prof. Dr. Erika Musterfrau, Prof. Dr. Max Mustermann'
+    }
+  ];
 
   constructor(private userService: UserService, private activatedRoute: ActivatedRoute, private router: Router) {
     this.organization$ = this.activatedRoute.params.pipe(
