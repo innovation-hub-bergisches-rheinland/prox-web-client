fd7719078b80d0ccd165ca5b0d5882f280814519
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-04-03 17:54:58 +0200

    feat: add project overview to user profile

--- src/app/modules/user/pages/user-profile-page/user-profile-page.component.ts
+++ src/app/modules/user/pages/user-profile-page/user-profile-page.component.ts
@@ -4,12 +4,15 @@ import { UserProfile } from '@data/schema/user-service.types';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '@data/service/user.service';
 import { KeycloakService } from 'keycloak-angular';
-import { map, tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 import { MatDialog } from '@angular/material/dialog';
 import {
   UserProfileEditorDialogComponent,
   UserProfileEditorInput
 } from '@modules/user/components/user-profile-editor-dialog/user-profile-editor-dialog.component';
+import { ProjectService } from '@data/service/project.service';
+import { ProjectWithAssociations } from '@data/schema/project-service.types';
+import { ProjectHistoryItem } from '@modules/profile/components/profile-project-history/profile-project-history-item/profile-project-history-item.component';
 
 @Component({
   selector: 'app-user-profile-page',
@@ -18,6 +21,8 @@ import {
 })
 export class UserProfilePageComponent implements OnInit {
   user$: Observable<UserProfile>;
+  offeredProjects$: Observable<ProjectWithAssociations[]>;
+  projectHistory$: Observable<ProjectHistoryItem[]>;
   avatar: string;
   id: string;
   isOwnProfile: boolean;
@@ -26,6 +31,7 @@ export class UserProfilePageComponent implements OnInit {
     private activatedRouter: ActivatedRoute,
     private userService: UserService,
     private keycloakService: KeycloakService,
+    private projectService: ProjectService,
     private router: Router,
     private dialog: MatDialog
   ) {
@@ -33,14 +39,29 @@ export class UserProfilePageComponent implements OnInit {
   }
 
   loadProfile() {
-    this.user$ = this.activatedRouter.params.pipe(
-      map(params => params.id),
+    const userId$: Observable<string> = this.activatedRouter.params.pipe(map(params => params.id));
+    this.user$ = userId$.pipe(
       tap(id => {
         this.id = id;
         this.isOwnProfile = this.keycloakService.getKeycloakInstance().subject === id;
         this.avatar = this.userService.getUserAvatar(id);
       }),
       mergeMap(id => this.userService.getUserProfile(id))
+    );
+    this.offeredProjects$ = userId$.pipe(mergeMap(id => this.projectService.findAvailableProjectsOfCreator(id, 'withAssociations')));
+    this.projectHistory$ = userId$.pipe(
+      mergeMap(id => this.projectService.findRunningAndFinishedProjectsOfCreator(id)),
+      map(projects =>
+        projects.map(project => {
+          return {
+            id: project.id,
+            title: project.name,
+            supervisor: project.supervisorName,
+            description: project.shortDescription
+          };
+        })
+      ),
+      catchError(err => [])
     );
   }
 
