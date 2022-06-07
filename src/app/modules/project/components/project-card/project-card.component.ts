76f4ee4fe5045327083c0dcc980d5e40f9802b31
Tobias Hund <tobias.hund@innovation-hub.de>
2022-06-07 07:02:08 +0200

    feat: show author in project card

--- src/app/modules/project/components/project-card/project-card.component.ts
+++ src/app/modules/project/components/project-card/project-card.component.ts
@@ -1,15 +1,23 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 import { ModuleType, Project, ProjectPermissions, Specialization } from '@data/schema/project-service.types';
 import { Tag } from '@data/schema/tag.resource';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { UserService } from '@data/service/user.service';
+import { Organization, UserProfile } from '@data/schema/user-service.types';
+import { map } from 'rxjs/operators';
+
+interface ProjectAuthor {
+  authorName: string;
+  profileRouter: string[];
+}
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.scss']
 })
-export class ProjectCardComponent {
+export class ProjectCardComponent implements OnInit {
   @Input()
   project: Project;
 
@@ -36,6 +44,31 @@ export class ProjectCardComponent {
 
   editIcon = faPen;
   deleteIcon = faTrash;
+
+  owner$: Observable<ProjectAuthor>;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    switch (this.project.owner.discriminator) {
+      case 'user':
+        this.owner$ = this.userService.getUserProfile(this.project.owner.id).pipe(
+          map(profile => ({
+            authorName: profile.name,
+            profileRouter: ['/users', this.project.owner.id]
+          }))
+        );
+        break;
+      case 'organization':
+        this.owner$ = this.userService.getOrganization(this.project.owner.id).pipe(
+          map(profile => ({
+            authorName: profile.name,
+            profileRouter: ['/organizations', this.project.owner.id]
+          }))
+        );
+        break;
+    }
+  }
 
   onDeleteClick() {
     this.delete.next(this.project);
