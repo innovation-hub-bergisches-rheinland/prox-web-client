7e2e9a18a2e80c37d22d5fd1efeffc556fc43055
Tobias Hund <tobias.hund@innovation-hub.de>
2023-03-08 13:32:07 +0100

    refactor: reuse color definitions and add default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,45 @@
 const colors = require('tailwindcss/colors');
 
+const ihbrBlue = {
+  50: '#e6eaf4',
+  100: '#c0cbe3',
+  200: '#96a9d0',
+  300: '#6c86bd',
+  400: '#4d6cae',
+  500: '#2d52a0',
+  600: '#284b98',
+  700: '#22418e',
+  800: '#1c3884',
+  900: '#112873',
+  DEFAULT: '#2d52a0'
+};
+const ihbrBerry = {
+  50: '#f6e6f2',
+  100: '#e9c1de',
+  200: '#da98c9',
+  300: '#cb6eb3',
+  400: '#bf4fa2',
+  500: '#9b1868',
+  600: '#ad2b8a',
+  700: '#a4247f',
+  800: '#9c1e75',
+  900: '#8c1363',
+  DEFAULT: '#9b1868'
+};
+const ihbrSky = {
+  50: '#e0f3fc',
+  100: '#b3e2f7',
+  200: '#80cff1',
+  300: '#4dbceb',
+  400: '#26ade7',
+  500: '#009fe3',
+  600: '#0097e0',
+  700: '#008ddc',
+  800: '#0083d8',
+  900: '#0072d0',
+  DEFAULT: '#009fe3'
+};
+
 /**
  * @type {import('tailwindcss/tailwind-config').TailwindConfig}
  */
@@ -11,9 +51,9 @@ module.exports = {
     themes: [
       {
         innohub: {
-          primary: '#9b1868',
-          secondary: '#2d52a0',
-          accent: '#009fe3',
+          primary: ihbrBerry.DEFAULT,
+          secondary: ihbrBlue.DEFAULT,
+          accent: ihbrSky.DEFAULT,
           neutral: '#2c2c2c',
           'base-100': '#FFFFFF',
           info: '#6599CD',
@@ -28,47 +68,14 @@ module.exports = {
     extend: {
       colors: {
         // InnoHub Blue
-        blue: {
-          50: '#e6eaf4',
-          100: '#c0cbe3',
-          200: '#96a9d0',
-          300: '#6c86bd',
-          400: '#4d6cae',
-          500: '#2d52a0',
-          600: '#284b98',
-          700: '#22418e',
-          800: '#1c3884',
-          900: '#112873'
-        },
+        blue: ihbrBlue,
         // InnoHub Berry
-        berry: {
-          50: '#f6e6f2',
-          100: '#e9c1de',
-          200: '#da98c9',
-          300: '#cb6eb3',
-          400: '#bf4fa2',
-          500: '#9b1868',
-          600: '#ad2b8a',
-          700: '#a4247f',
-          800: '#9c1e75',
-          900: '#8c1363'
-        },
+        berry: ihbrBerry,
         // InnoHub Light Blue
-        sky: {
-          50: '#e0f3fc',
-          100: '#b3e2f7',
-          200: '#80cff1',
-          300: '#4dbceb',
-          400: '#26ade7',
-          500: '#009fe3',
-          600: '#0097e0',
-          700: '#008ddc',
-          800: '#0083d8',
-          900: '#0072d0'
-        },
-        primary: '#9b1868',
-        secondary: '#2d52a0',
-        accent: '#009fe3'
+        sky: ihbrSky,
+        primary: ihbrBerry.DEFAULT,
+        secondary: ihbrBlue.DEFAULT,
+        accent: ihbrSky.DEFAULT
       },
       screens: {
         '3xl': '1921px'
