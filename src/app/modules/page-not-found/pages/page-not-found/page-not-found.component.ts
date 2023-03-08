1e1e269c72661d7c79f4dddd1f71a3d9d6acf07b
Tobias Hund <tobias.hund@innovation-hub.de>
2023-03-08 09:43:53 +0100

    feat: add noindex tag to 404 page

--- src/app/modules/page-not-found/pages/page-not-found/page-not-found.component.ts
+++ src/app/modules/page-not-found/pages/page-not-found/page-not-found.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss']
 })
-export class PageNotFoundComponent {}
+export class PageNotFoundComponent implements OnInit {
+  constructor(private meta: Meta) {}
+
+  ngOnInit(): void {
+    // Note that we are just adding the tag, but do not remove it.
+    // This is because the googlebot (and hopefully other bots) will
+    // only index the first page they see and follow the links individually.
+    // This should make it uneccessary to remove the tag.
+    this.meta.addTag({ name: 'robots', content: 'noindex' });
+  }
+}
