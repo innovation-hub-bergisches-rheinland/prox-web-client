1729e6346934cb6c6a0ad784704191617b28febf
Tobias Hund <tobias.hund@innovation-hub.de>
2023-06-26 15:51:30 +0200

    fix: fix slugging logic

--- src/app/data/service/tag.service.ts
+++ src/app/data/service/tag.service.ts
@@ -114,7 +114,6 @@ export class TagService {
       .toLowerCase()
       .normalize('NFD')
       .replace(/[\u0300-\u036f]/g, '')
-      .replace(/[^a-z0-9 -]/g, '')
       .replace(/\s+/g, '-')
       .replace(/-+/g, '-');
   }
