83971e991c6de24439cae9a212a7ea508f5e4b11
Jannik Alexander <jannik.alexander@innovation-hub.de>
2025-02-25 15:14:11 +0100

    feat(env): switch to temp url for testing + fix imprint
    Signed-off-by: Jannik Alexander <jannik.alexander@innovation-hub.de>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { Environment } from './environment.types';
 import { KeycloakConfig } from 'keycloak-js';
 
 const keycloakConfig: KeycloakConfig = {
-  url: 'https://login.aws.innovation-hub.de/auth',
+  url: 'https://login.realm-of-regret.de/auth',
   realm: 'innovation-hub-bergisches-rheinland',
   clientId: 'prox-web-client'
 };
@@ -11,7 +11,7 @@ export const environment: Environment = {
   name: 'PROD',
   production: true,
   keycloakConfig,
-  apiUrl: 'https://api.prox.aws.innovation-hub.de/v2',
+  apiUrl: 'https://api.prox.realm-of-regret.de/v2',
   enabledFeatures: ['proposal'],
   loggingConfig: {
     error: true,
